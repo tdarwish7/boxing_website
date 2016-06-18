9c5a76a6b5a534c7f79789f90067126295e53ccb
Teacher <Teacher>
2016-06-18 17:03:03 -0400

    edited index.js to link to contact me page

--- index.js
+++ index.js
@@ -7,4 +7,9 @@ server.get('/', function(request, response){
   response.sendFile('public/html/index.html', {root: __dirname});
 });
 
+server.get('/contact', function(request, response){
+  response.sendFile('public/html/contact.html', {root:__dirname});
+});
+
+
 server.listen(8080);
